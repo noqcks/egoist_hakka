e202b228567de505ce63f5decf7383bc4a1104d9
noqcks <benji@093b.org>
2024-04-07 22:10:02 +0000

    Upgrade dependencies

--- src/lib/urql-client.ts
+++ src/lib/urql-client.ts
@@ -7,6 +7,7 @@ import {
   ssrExchange,
 } from 'urql'
 import { cacheExchange } from '@urql/exchange-graphcache';
+import { makeDefaultStorage } from '@urql/exchange-graphcache';;
 import { makeDefaultStorage } from '@urql/exchange-graphcache/default-storage';
 
 export const createUrqlClient = () => {
